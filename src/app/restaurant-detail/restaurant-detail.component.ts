58c9ffc730af9da7d4dd375ae3b7ea20f621e9d8
Evandro <evandro@amee.com.br>
2017-09-25 17:51:24 -0300

    json-serve, restaurant detail

---
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core'
+
+//import { RestaurantsService } from '../restaurants/restaurants-service'
+
+@Component({
+  selector: 'mt-restaurant-detail',
+  templateUrl: './restaurant-detail.component.html'
+})
+export class RestaurantDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
